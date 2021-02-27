b87aa0b7eb4c1ac174609966914e9656f93a2083
Panawat Wong-klaew <pw443@cam.ac.uk>
2021-02-27 15:45:42 +0000

    Add simplified model function tex
    For fitting in the lag time domain at specific wavevector point

--- assets/geneqn.sh
+++ assets/geneqn.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
 cat ./modelv1.tex | pnglatex -o ./modelv1.png -O -s 12 -d 300 -e align\* -p mathtools,amssymb,amsthm
+cat ./modelv1_simplified.tex | pnglatex -o ./modelv1_simplified.png -O -s 12 -d 300 -e align\* -p mathtools,amssymb,amsthm
